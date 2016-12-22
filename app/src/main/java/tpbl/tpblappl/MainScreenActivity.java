419975d4df68a2e67dcb497469b65d527bbb69df
Mole80 <test@tutu.com>
2016-12-22 10:41:25 +0100

    first commit

---
+++ app/src/main/java/tpbl/tpblappl/MainScreenActivity.java
@@ -0,0 +1,53 @@
+//http://www.androidhive.info/2012/05/how-to-connect-android-with-php-mysql/
+
+package tpbl.tpblappl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends Activity
+{
+    Button btnViewProducts;
+    Button btnNewProduct;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+
+        // Buttons
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
+
+        // view products click event
+        btnViewProducts.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Launching All products Activity
+                Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // view products click event
+        btnNewProduct.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Launching create new product activity
+                Intent i = new Intent(getApplicationContext(), NewProductActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
